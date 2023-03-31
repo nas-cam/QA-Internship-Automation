145178524b59b399d0191a86a3d43fc1bce20739
Nasveta Čamdžić <nasveta.c@gmail.com>
2023-03-31 02:16:03 +0200

    Made changes based on mentor feedback

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -8,97 +8,46 @@ class LoginPage extends Page {
     /**
      * define selectors using getter methods
      */
-    get inputUsername() {
-        return $('//*[@id="username"]');
-    }
-
-    get inputPassword() {
-        return $('//*[@id="password"]');
-    }
-
-    get btnSubmit() {
-        return $('[name="login"]');
-    }
-    get header() {
-        return $('.noo-heading-content');
-    }
-    get logo() {
-        return $('img.custom-logo');
-    }
-    get product() {
-        return $('.noo-product-inner.noo-product-inner2');
-    }
-    get colorDropdown() {
-        return $('#pa_color');
-    }
-    get sizeDropdown() {
-        return $('#pa_size');
-    }
-    get btnAddToCart() {
-        return $('.single_add_to_cart_button.button.alt.wp-element-button');
-    }
-    get btnViewCart() {
-        return $('a.button.wc-forward.wp-element-button');
-    }
-    get tblCart() {
-        return $('.shop_table.shop_table_responsive.cart');
-    }
-    get btnProceedToCheckout() {
-        return $('.checkout-button.button.alt.wc-forward.wp-element-button');
-    }
-    get inputFirstName() {
-        return $('#billing_first_name');
-    }
-    get inputLastName(){
-        return $('#billing_last_name');
-    }
-    get inputCompany(){
-        return $('#billing_company');
-    }
-    get countryArrow(){
-        return $('.select2-selection__arrow');
-    }
-    get inputCountry(){
-        return $('#billing_country');
-    }
-    get inputBillingAddress(){
-        return $('#billing_address_1');
-    }
-
-    get inputTown(){
-        return $('#billing_city');
-    }
-    get selectBillingState(){
-        return $('#billing_state');
-    }
-    get inputPostCode(){
-        return $('#billing_postcode');
-    }
-    get inputPhone(){
-        return $('#billing_phone');
-    }
-    get inputEmail(){
-        return $('#billing_email');
-    }
-    get inputTerms(){
-        return $('#terms');
-    }
-    get btnPlaceOrder(){
-        return $('#place_order');
-    }
-    // get inputCountry(){
-    //     return $('.select2-search__field');
-    // }
+    get inputUsername() { return $('//*[@id="username"]'); }
+    get inputPassword() { return $('//*[@id="password"]'); }
+    get buttonSubmit() { return $('[name="login"]'); }
+    get header() { return $('.noo-heading-content'); }
+    get logo() { return $('img.custom-logo'); }
+    get product() { return $('.noo-product-inner.noo-product-inner2'); }
+    get colorDropdown() { return $('#pa_color'); }
+    get sizeDropdown() { return $('#pa_size'); }
+    get buttonAddToCart() { return $('.single_add_to_cart_button.button.alt.wp-element-button'); }
+    get buttonViewCart() { return $('a.button.wc-forward.wp-element-button'); }
+    get tblCart() { return $('.shop_table.shop_table_responsive.cart'); }
+    get buttonProceedToCheckout() { return $('.checkout-button.button.alt.wc-forward.wp-element-button'); }
+    get inputFirstName() { return $('#billing_first_name'); }
+    get inputLastName() { return $('#billing_last_name'); }
+    get inputCompany() { return $('#billing_company'); }
+    get countryArrow() { return $('.select2-selection__arrow'); }
+    get inputCountry() { return $('#billing_country'); }
+    get inputBillingAddress() { return $('#billing_address_1'); }
+    get inputTown() { return $('#billing_city'); }
+    get selectBillingState() { return $('#billing_state'); }
+    get inputPostCode() { return $('#billing_postcode'); }
+    get inputPhone() { return $('#billing_phone'); }
+    get inputEmail() { return $('#billing_email'); }
+    get inputTerms() { return $('#terms'); }
+    get buttonPlaceOrder() { return $('#place_order'); }
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    async login(username, password) {
+    async enterUsername(username){
+        await this.inputUsername.click();
         await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
     }
-
+    async enterPassword(password){
+        await this.inputUsername.click();
+        await this.inputUsername.setValue(password);
+    }
+    async clickOnSubmitButton(){
+        await this.buttonSubmit.click();
+    }
     /**
      * overwrite specific options to adapt it to page object
      */
@@ -122,99 +71,88 @@ class LoginPage extends Page {
     }
     async clickToSizeDropdown() {
         await this.sizeDropdown.click();
-
     }
     async selectSize(size) {
         await this.sizeDropdown.selectByVisibleText(size);
     }
     async clicktoAddToCartButton() {
-        await this.btnAddToCart.click();
+        await this.buttonAddToCart.click();
     }
-    async clickToViewCartBtn() {
-        await this.btnViewCart.click();
+    async clickToViewCartButton() {
+        await this.buttonViewCart.click();
     }
     async scrollToTblCart() {
         await this.tblCart.scrollIntoView();
     }
-    async clickToProceedToCheckoutbtn() {
-        await this.btnProceedToCheckout.click();
+    async clickToProceedToCheckoutButton() {
+        await this.buttonProceedToCheckout.click();
     }
-    async clickOnFirstnameField(){
-       await this.inputFirstName.click(); 
-       
+    async clickOnFirstnameField() {
+        await this.inputFirstName.click();
     }
-    async enterFirstName(firstname){
+    async enterFirstName(firstname) {
         await this.inputFirstName.setValue(firstname);
     }
-    async clickOnLastNameField(){
-        await this.inputLastName.click(); 
-        
-     }
-     async enterLastName(lastname){
-         await this.inputLastName.setValue(lastname);
-     }
-     async clickOnCompany(){
+    async clickOnLastNameField() {
+        await this.inputLastName.click();
+    }
+    async enterLastName(lastname) {
+        await this.inputLastName.setValue(lastname);
+    }
+    async clickOnCompany() {
         await this.inputCompany.click();
-     }
-     async enterCompany(company){
+    }
+    async enterCompany(company) {
         await this.inputCompany.setValue(company);
     }
-    async clickOnCountry(){
+    async clickOnCountry() {
         await this.countryArrow.click();
-     }
-     async enterCountry(country){
+    }
+    async enterCountry(country) {
         await this.inputCountry.selectByVisibleText(country);
     }
-    async clickOnAddress(){
-        await this.inputBillingAddress.click(); 
-        
-     }
-     async enterAddress(address){
-         await this.inputBillingAddress.setValue(address);
-     }
-     async clickOnTown(){
+    async clickOnAddress() {
+        await this.inputBillingAddress.click();
+    }
+    async enterAddress(address) {
+        await this.inputBillingAddress.setValue(address);
+    }
+    async clickOnTown() {
         await this.inputTown.click();
-     }
-     async enterTown(town){
+    }
+    async enterTown(town) {
         await this.inputTown.setValue(town);
-     }
-     async clickOnCountry(){
+    }
+    async clickOnCountry() {
         await this.countryArrow.click();
-     }
-     async enterState(state){
+    }
+    async enterState(state) {
         await this.selectBillingState.selectByVisibleText(state);
-     }
-     async clickOnPostCode(){
+    }
+    async clickOnPostCode() {
         await this.inputPostCode.click();
-     }
-     async enterPostCode(zip){
+    }
+    async enterPostCode(zip) {
         await this.inputPostCode.setValue(zip);
-     }
-
-     async clickOnPhone(){
+    }
+    async clickOnPhone() {
         await this.inputPhone.click();
-     }
-     async enterPhone (phone){
+    }
+    async enterPhone(phone) {
         await this.inputPhone.setValue(phone);
-     }
-     async clickOnEmail(){
+    }
+    async clickOnEmail() {
         await this.inputEmail.click();
-     }
-     async enterEmail(email){
+    }
+    async enterEmail(email) {
         await this.inputEmail.setValue(email);
-     }
-     async clickOnTerms(){
+    }
+    async clickOnTerms() {
         await this.inputTerms.click();
-     }
-     async clickOnPlaceOrderBtn(){
-        await this.btnPlaceOrder.click();
-     }
-
-
-
-
-
-
+    }
+    async clickOnPlaceOrderButton() {
+        await this.buttonPlaceOrder.click();
+    }
 }
 
 export default new LoginPage();
--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -9,28 +9,12 @@ class SecurePage extends Page {
     /**
      * define selectors using getter methods
      */
-    get flashAlert() {
-        return $('.noo-page-heading');
-    }
-    get homepageVal() {
-        return $('.navbar.navbar-default');
-    }
-    get productVal() {
-        return $('.noo-product-inner.noo-product-inner2');
-    }
-    get pageHeader() {
-        return $('.page-title');
-    }
-    get productColorPreview() {
-        return $('#pa_color');
-    }
-    get tblCartPreview() {
-        return $('.shop_table.shop_table_responsive.cart')
-    }
-    get thankYouNote(){
-        return $('.woocommerce');
-    }
-
+    get flashAlert() {return $('.noo-page-heading');}
+    get homepageVal() {return $('.navbar.navbar-default');}
+    get productVal() {return $('.noo-product-inner.noo-product-inner2');}
+    get pageHeader() {return $('.page-title');}
+    get productColorPreview() {return $('#pa_color');}
+    get tblCartPreview() {return $('.shop_table.shop_table_responsive.cart');}
+    get thankYouNote(){return $('.woocommerce');}
 }
-
 export default new SecurePage();
